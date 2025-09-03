5339fa04140b4758251de3c74a28eab315de72bd
HaiseFGC <kirijoker@hotmail.com>
2025-09-02 23:28:57 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,39 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule} from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Neo4jModule } from 'nest-neo4j';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'backend_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRES_HOST'),
+        port: config.get<number>('POSTGRES_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    Neo4jModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        scheme: config.get<string>('NEO4J_SCHEME'),
+        host: config.get<string>('NEO4J_HOST'),
+        port: config.get<number>('NEO4J_PORT'),
+        username: config.get<string>('NEO4J_USER'),
+        password: config.get<string>('NEO4J_PASSWORD'),
+      }),
     }),
   ],
   controllers: [AppController],
