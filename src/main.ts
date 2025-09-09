288b55ffe2490a41bd8e801dfb36da9be1b715c8
Fer666999 <fernando.aranda@alumnos.ucn.cl>
2025-09-09 15:51:19 -0300

    Cors habilitado

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   // habilita CORS para que el frontend pueda llamar al backend
+  app.enableCors({
+    origin: 'http://localhost:5173', // puerto donde corre tu frontend (Vite/React)
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
