945cd32daa3fc0a4106b8763cab2b64591695f4d
Anibal Gonzalez Veira <165087800+AnibalGonzalezV@users.noreply.github.com>
2025-09-24 20:28:28 -0300

    Limpieza

--- src/domain/entities/usuario.entity.ts
+++ src/domain/entities/usuario.entity.ts
@@ -2,7 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Carrera } from './carrera.entity';
 
 @Entity('usuarios')
-export class Usuario {
+export class Usuario { //REVISAR SI ES NECESARIO EL USUARIO COMPLETO. SOLO GUARDAREMOS RUT Y PROYECCIONES
   @PrimaryGeneratedColumn()
   id: number;
 
